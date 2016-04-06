d5ecc1a8e9e4dc27ed8e344c32acfe8bfd04f666
matthewrsj <mrsj@osuosl.com>
2016-04-06 13:07:37 -0700

    Add begin-rescue block so down clusters don't crash method

--- collectors.rb
+++ collectors.rb
@@ -21,29 +21,34 @@ class Collectors
     @cluster.each do |name|
       # for each cluster, append port number, endpoint, and query.
       uri = URI("https://#{name}.osuosl.bak:5080/2/instances?bulk=1")
-      Net::HTTP.start(uri.host, uri.port,
-                      use_ssl:     uri.scheme == 'https',
-                      verify_mode: OpenSSL::SSL::VERIFY_NONE) do |http|
-        # perform get request on full path.
-        response = http.request Net::HTTP::Get.new uri
-        # Store returned information in redis with datetime and node name
-        JSON.parse(response.body).each do |node|
-          node_name          = node['name']                     || 'unknown'
-          disk_sizes_meas    = node['disk.sizes']               || 'unknown'
-          disk_usage_meas    = node['disk_usage']               || 'unknown'
-          disk_template_meas = node['disk_template']            || 'unknown'
-          num_cpus_meas      = node['oper_vcpus']               ||
-                               node['beparams']['vcpus']        ||
-                               node['custom_beparams']['vcpus'] || 'unknown'
-          total_ram_meas     = node['beparams']['memory']       || 'unknown'
+      begin
+        Net::HTTP.start(uri.host, uri.port,
+                        use_ssl:     uri.scheme == 'https',
+                        verify_mode: OpenSSL::SSL::VERIFY_NONE) do |http|
+          # perform get request on full path. If this doesn't work, program
+          # control will jump to the rescue block below.
+          response = http.request Net::HTTP::Get.new uri
+          # Store returned information in redis with datetime and node name
+          JSON.parse(response.body).each do |node|
+            node_name          = node['name']                     || 'unknown'
+            disk_sizes_meas    = node['disk.sizes']               || 'unknown'
+            disk_usage_meas    = node['disk_usage']               || 'unknown'
+            disk_template_meas = node['disk_template']            || 'unknown'
+            num_cpus_meas      = node['oper_vcpus']               ||
+                                 node['beparams']['vcpus']        ||
+                                 node['custom_beparams']['vcpus'] || 'unknown'
+            total_ram_meas     = node['beparams']['memory']       || 'unknown'
 
-          @redis.mset(node_name, @template.result(binding),
-                      node_name + ':datetime', Time.new.inspect)
+            @redis.mset(node_name, @template.result(binding),
+                        node_name + ':datetime', Time.new.inspect)
+          end
+          # To retrieve the the node information, use redis.get and JSON.parse.
+          # This will give you a ruby hash of the node information.
+          # info = JSON.parse(@redis.get(<node_name>))
+          # time = @redis.get(<node_name> + ':datetime')
         end
-        # To retrieve the the node information, use redis.get and JSON.parse.
-        # This will give you a ruby hash of the node information.
-        # info = JSON.parse(@redis.get(<node_name>))
-        # time = @redis.get(<node_name> + ':datetime')
+      rescue SocketError
+        STDERR.puts "Uh oh, got a SocketError connecting to #{name}"
       end
     end
   end
