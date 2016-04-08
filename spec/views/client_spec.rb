4e5b59c52bccce17ee80e358a62d367d72713c17
Ken Lett <kennric@osuosl.org>
2016-04-08 16:44:07 -0700

    just check the response is ok

--- spec/views/client_spec.rb
+++ spec/views/client_spec.rb
@@ -2,7 +2,7 @@ require File.expand_path '../../spec_helper.rb', __FILE__
 
 describe 'The Client list endpoint' do
   include Rack::Test::Methods
-  
+
   let(:db) { Sequel.sqlite }
   before { Sequel::Migrator.run(db, "migrations") }
   after { db.disconnect }
@@ -15,8 +15,7 @@ describe 'The Client list endpoint' do
 
   it "provides a list of existing clients" do
   	get '/clients'
-
-	expect(User.top(2)).to have(2).item 
+  	expect(last_response).to be_ok
 
   end
 
