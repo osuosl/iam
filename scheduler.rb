79144cbdc33e73b5035a0fb05bb81a2bf93f4a20
Matthew Johnson <mrsj@osuosl.org>
2016-04-18 14:17:03 -0700

    Remove p statements from scheduler

--- scheduler.rb
+++ scheduler.rb
@@ -3,12 +3,10 @@ require_relative 'collectors'
 
 s = Rufus::Scheduler.new
 
-p [ :scheduled_at, Time.now ]
-
 s.every '30m', :first_in => 0.4 do
-    collector = Collectors.new
-    collector.collect_ganeti
-    p [ :success,  Time.now ]
+  # Collect ganeti node information every 30 minutes
+  collector = Collectors.new
+  collector.collect_ganeti
 end
 
 s.join
