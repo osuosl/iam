0423b4fd36cd086a78cc9f730cc83e680d0a7669
Alex Nguyen <alexlnguyen@gmail.com>
2016-05-06 16:38:40 -0700

    Added plugin report

--- plugins/disk_sizes/plugin.rb
+++ plugins/disk_sizes/plugin.rb
@@ -19,9 +19,25 @@ class DiskSize
   def collect
     # collect method should go here
   end
+  
+  SECONDS_IN_DAY = 60 * 60 * 24
+  def report(fqdn = '*', days = 1)
+    # setup time range
+    end_time = Time.now
+    start_time = Time.now - (days * SECONDS_IN_DAY)
 
-  def report
-    # report method should go here
+    # go into db table,
+    data_table = Iam.settings.DB[:measurementDiskSizes]
+    # if fqdn is default, return all
+    if fqdn == '*'
+      dataset = data_table.where(time: start_time..end_time)
+    # else return data filtered with fqdn name
+    else
+      dataset = data_table.where(node: fqdn)
+                          .where(time: start_time..end_time)
+    end
+    # format and make json/csv thing
+    puts dataset.all.to_json
   end
 end
 
---
+++ plugins/disk_sizes/test_data.rb
@@ -0,0 +1,47 @@
+# DB FILLING SCRIPT
+#
+#
+
+# ENV VARS
+require_relative '../../environment.rb'
+
+#DB CONNECT
+DB_Table = Iam.DB[:measurementDiskSizes]
+
+# BEFORE
+puts
+puts 'Before:'
+puts DB_Table.all
+puts '^BEFORE^^BEFORE^BEFORE^BEFORE^BEFORE^BEFORE^BEFORE^BEFORE'
+puts
+
+# MR.LOOPY LOOPHOLE
+(0..20).each do |i|
+  time_data = Time.now
+  node_data = 'FACEYMYBOOKY.com' + i.to_s
+  value_data = 1234 + rand(i)
+
+  DB_Table.insert(time: time_data,
+                  node: node_data,
+                  value: value_data
+                 )
+end
+sleep(2)
+
+# this time, do 10 reps with consistent node name
+(0..10).each do |i|
+  time_data = Time.now
+  node_data = 'FACEYMYBOOKY.com'
+  value_data = 1234 + rand(i)
+
+  DB_Table.insert(time: time_data,
+                  node: node_data,
+                  value: value_data
+                 )
+end
+
+# AFTER
+puts 'After:'
+puts DB_Table.all
+puts '^AFTER^AFTER^AFTER^AFTER^AFTER^AFTER^AFTER^AFTER^AFTER^AFTER'
+puts
