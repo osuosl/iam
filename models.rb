ef7bfab41ee8a447ae300a138b1cecd5ef754372
Ken Lett <kennric@osuosl.org>
2016-05-06 09:41:51 -0700

    require non-empty client name

--- models.rb
+++ models.rb
@@ -10,11 +10,10 @@ Sequel::Migrator.run(Iam.settings.DB, 'migrations') if ENV['RACK_ENV'] == 'test'
 # String    :description,   :text => true
 class Client < Sequel::Model
   one_to_many :projects
-  # uncomment to pass 'required fields' test
-  #def validate
-  #  super
-  #  errors.add(:name, 'cannot be empty') if !name || name.empty?
-  #end
+  def validate
+    super
+    errors.add(:name, 'cannot be empty') if !name || name.empty?
+  end
 end
 
 # Project data model
