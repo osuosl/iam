b35a7ae6b90719e5d603ee6f4be8b2749c8e9b36
matthewrsj <mrsj@osuosl.com>
2016-05-06 15:23:47 -0700

    Add schedule for disk_sizes

--- scheduler.rb
+++ scheduler.rb
@@ -1,5 +1,7 @@
 require 'rufus/scheduler'
+require 'redis'
 require_relative 'collectors'
+require_relative 'plugins/disk_sizes/plugin.rb'
 
 s = Rufus::Scheduler.new
 
@@ -7,6 +9,17 @@ s.every '30m', :first_in => 0.4 do
   # Collect ganeti node information every 30 minutes
   collector = Collectors.new
   collector.collect_ganeti
+end
+
+# Change '15m' on next line to 4 to test
+s.every '30m', :first_in => '15m' do
+  redis = Redis.new(:host => ENV['REDIS_HOST'])
+  DiskSize.new.register
+  redis.keys().each do |key|
+    if !key.end_with?(':datetime')
+      DiskSize.new.store key
+    end
+  end
 end
 
 s.join
