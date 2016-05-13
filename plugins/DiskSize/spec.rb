ae450bad371c5064e8e361945685c5c3b957e98c
matthewrsj <mrsj@osuosl.com>
2016-05-13 09:57:55 -0700

    Remove nodes inserted in DB by spec tests
    I should follow my own advice

--- plugins/DiskSize/spec.rb
+++ plugins/DiskSize/spec.rb
@@ -1,61 +1,98 @@
 require_relative './plugin.rb'
 describe DiskSize do
-  # register method
+  before(:all) do
+    @redis = Redis.new(host: ENV['REDIS_HOST'])
+  end
+
   it '.register does not raise an error when invoked' do
     expect { DiskSize.new.register }.to_not raise_error
   end
 
   it '.register actually actually creates a disk_size_measurements table' do
+    # Table shouldn't exist before registration
     expect do
       Iam.settings.DB.table_exists?(:disk_size_measurements).to be_false
     end
+
     DiskSize.new.register
+
+    # Table should exist after registration
     expect { Iam.settings.DB.table_exists?(:disk_size_measurements).to be_true }
   end
 
   # Store method
   it '.store does not fail with valid data' do
-    redis = Redis.new(host: ENV['REDIS_HOST'])
-    redis.mset('nodename', JSON.generate(disk_sizes: '[10,20]', active: true),
-               'nodename:datetime', DateTime.now)
+    # Put some data in redis for store method
+    @redis.mset('nodename', JSON.generate(disk_sizes: '[10,20]', active: true),
+                'nodename:datetime', DateTime.now)
+
+    # Register DiskSize plugin
     DiskSize.new.register
+
+    # Store redis nodes in DB, no error
     expect { DiskSize.new.store('nodename') }.to_not raise_error
+
+    # Check that store actually stored the node
     expect do
       Iam.settings.DB[:disk_size_measurements].where(node: 'nodename').to exist
     end
-    redis.del('nodename')
-    redis.del('nodename:datetime')
+
+    # Reset redis and DB
+    @redis.del('nodename')
+    @redis.del('nodename:datetime')
+    Iam.settings.DB[:disk_size_measurements].where(
+      node: %w(nodename nodename:datetime)).delete
   end
 
   it '.store fails when not passed node name' do
+    # This is bad plugin usage that should actually crash
     expect { DiskSize.new.store }.to raise_error(ArgumentError)
   end
 
   it '.store does not crash when passed improperly formatted data' do
-    redis = Redis.new(host: ENV['REDIS_HOST'])
-    redis.mset('badnode', JSON.generate(disk_size: '[10,20]', active: true),
-               'goodnode', JSON.generate(disk_sizes: '[10,20]', active: true))
+    # Put some data in redis for store method
+    @redis.mset('badnode', JSON.generate(disk_size: '[10,20]', active: true),
+                'goodnode', JSON.generate(disk_sizes: '[10,20]', active: true))
+
+    # Register DiskSize plugin
     DiskSize.new.register
+
+    # Don't crash on bad info, but don't store anything either
     expect { DiskSize.new.store('badnode') }.to_not raise_error
     dataset = Iam.settings.DB[:disk_size_measurements]
     expect { dataset.where(node: 'badnode').to not_exist }
 
+    # Store good info
     DiskSize.new.store('goodnode')
     expect { dataset.where(node: 'goodnode').to exist }
-    redis.del('badnode')
-    redis.del('goodnode')
+
+    # Reset redis and DB
+    @redis.del('badnode')
+    @redis.del('goodnode')
+    Iam.settings.DB[:disk_size_measurements].where(
+      node: %w(badnode goodnode)).delete
   end
 
   it '.store properly sums all disk sizes when storing in DB' do
-    redis = Redis.new(host: ENV['REDIS_HOST'])
-    redis.mset('nodename', JSON.generate(disk_sizes: '[10,20]', active: true),
-               'nodename:datetime', DateTime.now)
+    # Put some data in redis for store method
+    @redis.mset('nodename', JSON.generate(disk_sizes: '[10,20]', active: true),
+                'nodename:datetime', DateTime.now)
+
+    # Register DiskSize plugin
     DiskSize.new.register
+
+    # Store node data
     dataset = Iam.settings.DB[:disk_size_measurements]
     DiskSize.new.store('nodename')
+
+    # Make sure store method properly summed disk sizes
     expect(dataset.where(node: 'nodename').get(:value)).to eq(30)
-    redis.del('nodename')
-    redis.del('nodename:datetime')
+
+    # Reset redis and DB
+    @redis.del('nodename')
+    @redis.del('nodename:datetime')
+    Iam.settings.DB[:disk_size_measurements].where(
+      node: %w(nodename nodename:datetime)).delete
   end
 
   # Report method
