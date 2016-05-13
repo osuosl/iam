2869f363cf53d520041fc74c7bc09b4868660ab1
matthewrsj <mrsj@osuosl.com>
2016-05-13 12:14:58 -0700

    Polish store method

--- plugins/DiskSize/plugin.rb
+++ plugins/DiskSize/plugin.rb
@@ -5,6 +5,8 @@ require_relative '../../models.rb'
 # Disk Sizes data plugin
 class DiskSize
   def initialize
+    @redis = Redis.new(host: ENV['REDIS_HOST'])
+    @dataset = Iam.settings.DB
     register
   end
 
@@ -20,26 +22,17 @@ class DiskSize
   end
 
   def store(fqdn)
-    # Get node_resources id that matches fqdn
-    resource_id = Iam.settings.DB[:node_resources].where(name: fqdn).get(:id)
-
-    # Pull from our cache
-    redis = Redis.new(host: ENV['REDIS_HOST'])
+    # Pull node information from redis as a ruby hash
+    node_info = JSON.parse(@redis.get(fqdn))
 
     # Insert data into disk_size_measurements table
-    dataset = Iam.settings.DB[:disk_size_measurements]
-
-    node_info = JSON.parse(redis.get(fqdn))
-
-    # Remove brackets then split on ',' to create an array
-    disk_sizes = node_info['disk_sizes'].tr('[]', '').split(',').map(&:to_i)
-
-    # Insert data into disk_size_measurements table
-    dataset.insert(node:          fqdn,
-                   value:         disk_sizes.inject(0, :+),
-                   active:        node_info['active'],
-                   created:       DateTime.now,
-                   node_resource: resource_id)
+    @dataset[:disk_size_measurements].insert(
+      node:          fqdn,
+      value:         node_info['disk_sizes']
+                       .tr('[]', '').split(',').map(&:to_i).inject(0, :+),
+      active:        node_info['active'],
+      created:       DateTime.now,
+      node_resource: @dataset[:node_resources].where(name: fqdn).get(:id))
   rescue => e        # Don't crash on errors
     STDERR.puts e    # Log the error
   end
