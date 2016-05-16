cc94cf95e8fc126fbee7a11d491392fe8ffeb51b
matthewrsj <mrsj@osuosl.com>
2016-05-16 16:00:51 -0700

    Start work on updating scheduler

--- scheduler.rb
+++ scheduler.rb
@@ -3,6 +3,7 @@ require 'redis'
 require_relative 'collectors'
 require_relative 'plugins/disk_sizes/plugin.rb'
 
+redis = Redis.new(host: ENV['REDIS_HOST'])
 s = Rufus::Scheduler.new
 
 s.every '30m', first_in: 0.4 do
@@ -13,11 +14,20 @@ end
 
 # Change '15m' on next line to 4 to test
 s.every '30m', first_in: 4 do
-  redis = Redis.new(host: ENV['REDIS_HOST'])
-  DiskSize.new.register
-  redis.keys.each do |key|
-    DiskSize.new.store key unless key.end_with?(':datetime')
+  Iam.settings.DB[:plugins].get(:name).each do |plugin|
+    puts plugin
+    require_relative "plugins/#{plugin}/plugin.rb"
+    redis.keys.each do |key|
+      Object.const_get(plugin).store key unless key.end_with?(':datetime')
+    end
   end
 end
+
+
+#  DiskSize.new.register
+#  redis.keys.each do |key|
+#    DiskSize.new.store key unless key.end_with?(':datetime')
+#  end
+#end
 
 s.join
