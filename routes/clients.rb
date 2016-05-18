cf9e902e1956217d0de1d528d72abc6384498935
Elijah C. Voigt <voigte@osuosl.org>
2016-05-18 11:42:50 -0700

    Initial movement of `routes` methods to `routes/`
    Routes now live in `routes/*.rb`
    To add a new route define a module as such:
    ```ruby
    module Sinatra
      module MyNewRoutes
        def self.registered(app)
          app.get '/...' do
            ...
          end
          app.post ...
          ...
        end
      end
      register MyNewRoutes
    end
    ```
    
    and in main.rb add:
    ```ruby
      register MyNewRoutes`
    ```
    below the existing registers.
    
    refs #69

---
+++ routes/clients.rb
@@ -0,0 +1,65 @@
+require 'sinatra/base'
+
+module Sinatra
+  module ClientRoutes
+    def self.registered(app)
+      ##
+      # Clients
+      ##
+      app.get '/clients/new' do
+        # get new client form
+        erb :'clients/edit'
+      end
+
+      app.get '/clients/:id' do
+        # view a client
+        @client = Client[id: params[:id]]
+        halt 404, 'Client not found' if @client.nil?
+        erb :'clients/show'
+      end
+
+      app.get '/clients/:id/edit' do
+        # get client edit form
+        @client = Client[id: params[:id]]
+        halt 404, 'Client not found' if @client.nil?
+        erb :'clients/edit'
+      end
+
+      app.get '/clients' do
+        # get a list of all clients
+        @clients = Client.each { |x| p x.name }
+        erb :'clients/index'
+      end
+
+      # This could also be PUT
+      app.post '/clients' do
+        # recieve new client
+        client = Client.create(name: params[:name],
+                               description: params[:description] || '',
+                               contact_email: params[:contact_name] || '',
+                               contact_name: params[:contact_email] || '')
+        redirect "/clients/#{client.id}"
+      end
+
+      app.patch '/clients' do
+        # recieve an updated client
+        client = Client[id: params[:id]]
+
+        client.update(name: params[:name] || client.name,
+                      description: params[:description] || client.description,
+                      contact_email: params[:contact_name] || client.contact_name,
+                      contact_name: params[:contact_email] || client.contact_email)
+        redirect "/clients/#{params[:id]}"
+      end
+
+      app.delete '/clients/:id' do
+        # delete a client
+        @client = Client[id: params[:id]]
+        @client.delete unless @client.nil?
+        redirect '/clients' unless @client.nil?
+        404
+      end
+    end
+  end
+  register ClientRoutes
+end
