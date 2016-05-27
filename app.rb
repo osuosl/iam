2741459b91ac21e355af4a301606bc7861d78709
Alex Nguyen <alexlnguyen@gmail.com>
2016-05-27 15:02:15 -0700

    Added /node CRUD routes in app.rb, still need erb files

--- app.rb
+++ app.rb
@@ -148,4 +148,69 @@ class Iam < Sinatra::Base
     404
   end
 
+  ##
+  # Node Resource
+  ##
+
+  get '/node/new' do
+    # get new node form
+    erb :'node/edit'
+  end
+
+  get '/node/:id' do
+    # view a node
+    @node = NodeResource[id: params[:id]]
+    halt 404, 'node not found' if @node.nil?
+    erb :'node/show'
+  end
+
+  get '/node/:id/edit' do
+    # get node edit form
+    @node = NodeResource[id: params[:id]]
+    halt 404, 'node not found' if @node.nil?
+    erb :'node/edit'
+  end
+
+  get '/nodes' do
+    # get a list of all node
+    @node = NodeResource.all
+    erb :'clients/index'
+  end
+
+  # This could also be PUT
+  post '/nodes' do
+    # recieve new node
+    node = NodeResource.create(project_id: Iam.settings.DB[:projects]
+                                            .where(name: params[:project_name])
+                                            .get(:id) || node.project_id,
+                               name:       params[:name],
+                               type:       params[:type] || '',
+                               cluster:    params[:cluster] || '',
+                               created:    DateTime.now || '',
+                               modified:   DateTime.now || '')
+    redirect "/node/#{node.id}"
+  end
+
+  patch '/nodes' do
+    # recieve an updated node
+    node = NodeResource[id: params[:id]]
+
+    node.update(project_id: Iam.settings.DB[:projects]
+                              .where(name: params[:project_name])
+                              .get(:id) || node.project_id,
+                name:       params[:name] || node.name,
+                type:       params[:type] || node.type,
+                cluster:    params[:cluster] || node.cluster,
+                created:    params[:created] || node.created,
+                modified:   params[:modified] || node.modified)
+    redirect "/node/#{params[:id]}"
+  end
+
+  delete '/node/:id' do
+    # delete a node
+    @node = NodeResource[id: params[:id]]
+    @node.delete unless @node.nil?
+    redirect '/nodes' unless @node.nil?
+    404
+  end
 end
