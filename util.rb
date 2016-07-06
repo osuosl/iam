da21d3e0761787497241457032761a5d3f355d35
cthalmann <cthalmann@osuosl.org>
2016-07-06 14:23:49 -0700

    Added more tests.

--- util.rb
+++ util.rb
@@ -3,20 +3,56 @@ require_relative 'environment'
 
 # IAM - a resource usage metric collection and reporting system
 class Util
-
-  def self.maxValue(data)
-    # write code here to find the max value
-    return 80
-  end
-  def self.minValue(data)
-    # write code here to find the max value
-    min = data[0][:value]
-    for number in data
-      compareMin = number[:value]
-      if min > compareMin
-        min = compareMin
+  def self.max_value(data)
+    if data.empty?
+      return 0
+    end
+    max = data[0][:value]
+    data.each do |item|
+      compare_max = item[:value]
+      if max < compare_max
+        max = compare_max
       end
-    end 
+    end
+    return max
+  end
+  def self.min_value(data)
+    return 0 if data.empty?
+    min = data[0][:value]
+    data.each do |item|
+      compare_min = item[:value]
+      if min > compare_min
+        min = compare_min
+      end
+    end
     return min
+  end
+  def self.average_value(data)
+    if data.empty?
+      return 0
+    end
+    average = data[0][:value]
+    count = 1
+    data.slice(1..-1).each do |item|
+      average += item[:value]
+      count += 1
+    end
+    average = average / count
+    return average
+  end
+  def self.is_positive_integer_value(data)
+    flag = true
+    i = 0
+    while i < data.length && flag != false
+      if data[i][:value].is_a? Integer
+        if data[i][:value] < 0
+          flag = false
+        end
+      else
+        flag = false
+      end
+      i += 1
+    end
+    return flag
   end
 end
