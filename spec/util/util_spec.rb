81f2ff15d031636de2f1591bb39768c4699dfc01
cthalmann <cthalmann@osuosl.org>
2016-07-06 14:23:49 -0700

    Added more tests.

--- spec/util/util_spec.rb
+++ spec/util/util_spec.rb
@@ -6,17 +6,13 @@ describe 'The Utility tests' do
   def app
     Iam
   end
-
   include Rack::Test::Methods
-
   before(:all) do
     # anything that should happen before all tests
-    
     # data from report method from plugin
     # to test for max: 80
     # to test for min: 2
     # to test for average: 25
-
     @data_max_80 = [
       { id: 1, node_resource: nil,
         created: Time.parse('2016-07-01 21:43:27 +0000'),
@@ -31,33 +27,69 @@ describe 'The Utility tests' do
         created: Time.parse('2016-07-01 21:43:27 +0000'),
         node: 'cthalmann.osuosl.org', value: 4, active: true }
     ]
-
+    @data_empty = []
+  end
+  it 'returns the max value' do
+    expect(Util.max_value(@data_max_80)).to eq(80)
   end
 
-  
-  it 'returns the max value' do
-    expect(Util.maxValue(@data_max_80)).to eq(80)
+  it 'returns 0 for max value if array is empty' do
+    expect(Util.max_value(@data_empty)).to eq(0)
   end
 
   it 'returns the min value' do
-    expect(Util.minValue(@data_max_80)).to eq(2)
+    expect(Util.min_value(@data_max_80)).to eq(2)
+  end
+
+  it 'returns 0 for min value if array is empty' do
+    expect(Util.min_value(@data_empty)).to eq(0)
   end
 
   it 'returns the average value' do
-    expect(Util.averageValue(@data_max_80).to eq(25)
+    expect(Util.average_value(@data_max_80)).to eq(25)
   end
-  # Test ideas:
-  # date
-  # => is date after today's date?
-  # => is date earlier than date range?
-  # values
-  # => is value negative?  Should it be?
-  # => is value greater than max?  Does it become new max?
-  # => same for min
 
-  # it 'says hello' do
-  #   get '/'
-  #   expect(last_response).to be_ok
-  #   expect(last_response.body).to eq('Hello')
-  # end
+  it 'returns 0 for average of values if array is empty' do
+    expect(Util.average_value(@data_empty)).to eq(0)
+  end
+
+  it 'returns false if the value of an item in the array is not
+  a positive integer' do
+
+    @data_set_false = [
+      { id: 1, node_resource: nil,
+        created: Time.parse('2016-07-01 21:43:27 +0000'),
+        node: 'alembic-java.osuosl.org', value: 14, active: true },
+      { id: 2, node_resource: nil,
+        created: Time.parse('2016-07-01 21:43:27 +0000'),
+        node: 'amahi.osuosl.org', value: -2, active: true },
+      { id: 3, node_resource: nil,
+        created: Time.parse('2016-07-01 21:43:28 +0000'),
+        node: 'answers.ros.osuosl.org', value: 80, active: true },
+      { id: 4, node_resource: nil,
+        created: Time.parse('2016-07-01 21:43:27 +0000'),
+        node: 'cthalmann.osuosl.org', value: 4, active: true }
+    ]
+
+    expect(Util.is_positive_integer_value(@data_set_false)).to eq(false)
+  end
+
+  it 'returns true if all the values of all the items in the array are
+  positive integers' do
+    @data_set_true = [
+      { id: 1, node_resource: nil,
+        created: Time.parse('2016-07-01 21:43:27 +0000'),
+        node: 'alembic-java.osuosl.org', value: 14, active: true },
+      { id: 2, node_resource: nil,
+        created: Time.parse('2016-07-01 21:43:27 +0000'),
+        node: 'amahi.osuosl.org', value: 2, active: true },
+      { id: 3, node_resource: nil,
+        created: Time.parse('2016-07-01 21:43:28 +0000'),
+        node: 'answers.ros.osuosl.org', value: 80, active: true },
+      { id: 4, node_resource: nil,
+        created: Time.parse('2016-07-01 21:43:27 +0000'),
+        node: 'cthalmann.osuosl.org', value: 4, active: true }
+    ]
+    expect(Util.is_positive_integer_value(@data_set_true)).to eq(true)
+  end
 end
