167227fbe1ab5222d9013391a402a9437b93c73f
hsolorzano <hsolorzano@osuosl.org>
2016-08-15 15:19:05 -0700

    Add comments to logs

--- logging/logs.rb
+++ logging/logs.rb
@@ -1,9 +1,16 @@
+# Types of log statements:
+#   log.debug "a very nice little debug message"
+#   log.info "things are operating nominally"
+#   log.warn "this is your last warning"
+#   log.error StandardError.new("something went horribly wrong")
+#   log.fatal "I Die!"
+
 require 'logging'
 require 'environment.rb'
 
-# Creates a logfile with the level 'debug'
+# Creates logfile with the level 'debug'
 #   that appends to log_file.log
-log = Logging.logger['log_file']
+log = Logging.logger['logfile']
 log.level = :debug
 log.add_appenders \
    Logging.appenders.file('log_file.log')
