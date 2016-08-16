74614720af13fb1f88182332b8e8a2183d202a9e
hsolorzano <hsolorzano@osuosl.org>
2016-08-16 11:56:25 -0700

    Create env variable

--- logging/logs.rb
+++ logging/logs.rb
@@ -7,13 +7,13 @@
 
 require 'logging'
 require 'environment.rb'
-
+@file = ENV['logging/log_file.log']
 # Creates logfile with the level 'debug'
 #   that appends to log_file.log
 log = Logging.logger['logfile']
 log.level = :debug
 log.add_appenders \
-   Logging.appenders.file('logging/log_file.log')
+   Logging.appenders.file(@file)
 
 log.debug 'Created logger'
 log.info 'Program started'
