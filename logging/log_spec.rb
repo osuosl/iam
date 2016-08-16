f882d457ad8e3d9b9f13caa38a89647bdd524ade
hsolorzano <hsolorzano@osuosl.org>
2016-08-16 14:58:40 -0700

    Fix test

--- logging/log_spec.rb
+++ logging/log_spec.rb
@@ -1,17 +1,27 @@
 require 'logging'
+require 'rspec'
+require 'rspec/logging_helper'
 
 describe 'The Logging application' do
   def app
     Iam
   end
-  before(:all) do
-    ::Logging.init
-    @levels = ::Logging::LEVELS
-    @event = ::Logging::LogEvent.new('logger', @levels['debug'],
-    'testOutput', false)
-  end
 
-  it 'should output correct message' do
-    expect(@event[:data]).to eq('testOutput')
+# Configure RSpec to capture log messages for each test. The output from the
+# logs will be stored in the @log_output variable. It is a StringIO instance.
+RSpec.configure do |config|
+  include RSpec::LoggingHelper
+  config.capture_log_messages
+end
+
+# Now within your specs you can check that various log events were generated.
+  it 'should be able to read a log message' do
+    logger = Logging.logger['SuperLogger']
+
+    logger.debug'foo bar'
+    logger.warn  'just a little warning'
+
+    expect(@log_output.readline).to eq("DEBUG  SuperLogger : foo bar\n")
+    expect(@log_output.readline).to eq(" WARN  SuperLogger : just a little warning\n")
   end
 end
