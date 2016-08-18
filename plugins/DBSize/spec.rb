1d0de06952ad1f6ca13c8620d5285cfb6ad15efc
Alex Nguyen <alexlnguyen@gmail.com>
2016-08-18 14:48:48 -0700

    fixed so tests run

--- plugins/DBSize/spec.rb
+++ plugins/DBSize/spec.rb
@@ -13,7 +13,7 @@ describe 'DBSize plugin' do
     end
 
     before(:each) do
-      @cache.set('test_db', db_key: "123456.0", active: true)
+      @cache.set('test_db', 'Data Base Size in Bytes': "123456.0", active: true)
       @cache.write
     end
 
@@ -26,15 +26,15 @@ describe 'DBSize plugin' do
     it 'does not fail with valid data' do
       # Store cached nodes in DB, no error
       expect { DBSize.new.store('test_db') }.to_not raise_error
-
+      puts @db_table.where(db: 'test_db')
       # Check that store actually stored the node
-      expect(@db_table.where(node: 'test_db')).to_not be_empty
+      expect(@db_table.where(db: 'test_db')).to_not be_empty
     end
 
     it 'fails when not passed node name' do
       # This is bad plugin usage that should actually crash
       expect { DBSize.new.store }.to raise_error(ArgumentError)
     end
-    
+
   end
 end
