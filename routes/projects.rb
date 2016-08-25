75c387de3992f1cadfc41813318cde8f99782104
hsolorzano <hsolorzano@osuosl.org>
2016-08-25 09:17:13 -0700

    Fix edit, show, and index

--- routes/projects.rb
+++ routes/projects.rb
@@ -19,6 +19,9 @@ module Sinatra
           MyLog.log.fatal 'routes/projects: Project not found'
           halt 404, 'Project not found'
         end
+        puts "hihhiih"
+        puts @project.client_id
+        @clients = Client.filter(id: @project.client_id).all
         erb :'projects/show'
       end
 
@@ -41,26 +44,38 @@ module Sinatra
       # This could also be PUT
       app.post '/projects/?' do
         # recieve new project
-        project = Project.create(name:        params[:name],
-               client_id:   Iam.settings.DB[:clients]
-                  .where(name: params[:client_name])
-                  .get(:id) || '',
-               resources:   params[:resources] || '',
-               description: params[:description] || '')
-        redirect "/projects/#{project.id}"
+        project = Project.create(name: params[:name],
+                                 client_id:   Iam.settings.DB[:clients]
+                                            .where(name: params[:client_name])
+                                            .get(:id) || '',
+                                 resources:   params[:resources] || '',
+                                 description: params[:description] || ''
+                                 )
+        redirect "/projects/#{ project.id }"
       end
 
       app.patch '/projects/?' do
+        # set blanks to nil
+        params[:name] = nil if params[:name] == ''
+        params[:resources] = nil if params[:resources] == ''
+        params[:description] = nil if params[:description] == ''
+        params[:active] = nil if params[:description] == ''
+
         # recieve an updated project
         project = Project[id: params[:id]]
-        project.update(name:        params[:name] || project.name,
-           client_id:   Iam.settings.DB[:clients]
-              .where(name: params[:client_name])
-               .get(:id), #|| project.client_id,
-           resources:   params[:resources] || project.resources,
-           description: params[:description] || project.description,
-           active: params[:active] || project.active)
+        project.update(name:  params[:name] || project.name,
+                       resources:   params[:resources] || project.resources,
+                       description: params[:description] || project.description,
+                       active: params[:active] || project.active)
         redirect "/projects/#{params[:id]}"
+      end
+
+      app.delete '/clients/:id/?' do
+        # delete a client
+        project = Project[id: params[:id]]
+        project.delete unless project.nil?
+        redirect '/projects' unless project.nil?
+        404
       end
     end
   end
