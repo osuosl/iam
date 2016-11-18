a65b27439b656f49f66fd15f2a08071b2f0f8941
Ken Lett <kennric@osuosl.org>
2016-11-18 10:51:38 -0800

    use correct key for cache

--- plugins/DBSize/plugin.rb
+++ plugins/DBSize/plugin.rb
@@ -23,13 +23,12 @@ class DBSize < BasePlugin
   def store(db_host)
     # Pull node information from cache as a ruby hash
     db_info = @cache.get(db_host)
-    db_key = 'Data Base Size in Bytes'
 
     # Check for valid data
-    if db_info[db_key].nil? || db_info[db_key] == ''
+    if db_info[db_host].nil? || db_info[db_key] == ''
       MyLog.log.warn "DBSize: No DBSize information for #{db_host}"
       raise "No DBSize information for #{db_host}\n"
-    elsif !db_info[db_key].is_a? Numeric
+    elsif !db_info[db_host].is_a? Numeric
       MyLog.log.warn "DBSize: DB information for #{db_host} \
         malformed (should be a number)"
       raise "DB information for #{db_host} malformed (should be a number)\n"
@@ -38,7 +37,7 @@ class DBSize < BasePlugin
     # Insert data into db_size_measurements table
     @database[@table].insert(
       db:            db_host,
-      value:         db_info[db_key],
+      value:         db_info[db_host],
       active:        1,
       created:       DateTime.now,
       node_resource: @database[:node_resources].where(name: db_host).get(:id)
