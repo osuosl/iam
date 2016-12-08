f65e92a3fdfc18a1d846066e22bb2d41d3835581
Ken Lett <kennric@osuosl.org>
2016-12-08 10:30:15 -0800

    migration is necessary to rename column

---
+++ plugins/DBSize/migrations/002_migrations.rb
@@ -0,0 +1,10 @@
+# DBSize's migration file
+
+Sequel.migration do
+  transaction
+  change do
+    alter_table(:db_size_measurements) do # Plugin's DB model
+      rename_column :node_resource, :db_resource
+    end
+  end
+end
