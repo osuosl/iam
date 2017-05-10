a6bf2d8dcfd8e8c52a472d92a46c5ffc37e0035b
Devon Cash <cashd@crimson.workstation.cass.oregonstate.edu>
2017-05-10 15:44:37 -0700

    initial pass at chef collector

---
+++ spec/collectors/chef_spec.rb
@@ -0,0 +1,74 @@
+# frozen_string_literal: true
+require_relative '../../collectors.rb'
+require_relative '../spec_helper.rb'
+require 'chef_zero/server'
+
+describe 'IaM Chef Collector' do
+  include Rack::Test::Methods
+  # Create new chef server in memory
+  server = ChefZero::Server.new
+
+  before :all do
+    # Start the chef server
+    server.start_background
+
+    # Define test data
+    test_data = {
+      'nodes' => {
+        'testNode1' => {
+          'name' => 'testNode1',
+          'automatic' => {
+            'cpu' => {
+              'total' => 2,
+              'real' => 1
+            },
+            'memory' => {
+              'total' => '501924kB',
+              'free' => '196504kB'
+            }
+          }
+        }
+      }
+    }
+
+    @expected = []
+    # Populate the expected array the way as the collector
+    # The actual return data will be in JSON format, so the
+    # access will be slightly different
+    test_data['nodes'].each do |_key, val|
+      @expected.push(
+        val['name'] => {
+          'disk_size' => val['automatic']['memory']['total'],
+          'disk_free' => val['automatic']['memory']['free'],
+          'num_cpus'  => val['automatic']['cpu']['total'],
+          'num_rcpus' => val['automatic']['cpu']['real']
+        }
+      )
+    end
+
+    # Load data onto test server
+    server.load_data(test_data)
+  end
+
+  after :all do
+    # Clear server data
+    server.clear_data
+    # Stop the chef server
+    server.stop
+  end
+
+  it 'Correctly reads and stores data' do
+    # c = Collectors.new
+    # call chef collector
+    # c.collect_chef(
+    #   server.url,
+    #   ENV['TEST_CHEF_USER'],
+    #   ENV['TEST_CHEF_PEM']
+    # )
+    cache = Cache.new("#{Iam.settings.cache_path}/chef_cache")
+
+    @expected.each do |var|
+      expect(cache.dump).to include(var)
+    end
+  end
+end
